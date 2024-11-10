3cc9deca8b0a0d5f027a02fdc03018809a43ecc3
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2024-11-10 14:39:22 +0100

    Add service activation to state

--- src/main.tf
+++ src/main.tf
@@ -6,8 +6,16 @@ provider "google" {
   project = var.project_id
 }
 
+resource "google_project_service" "firestore" {
+  service = "firestore.googleapis.com"
+}
+
 resource "google_firestore_database" "this" {
   name        = "(default)"
   location_id = "europe-west1"
   type        = "FIRESTORE_NATIVE"
+
+  depends_on = [
+    google_project_service.firestore
+  ]
 }
